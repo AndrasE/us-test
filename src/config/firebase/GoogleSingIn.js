005bf147b4576b3e8e7ca5860f079f49f4c8c423
Andras E <rohadtsajt@gmail.com>
2023-02-26 10:17:05 +0000

    g-auth-test

--- src/config/firebase/GoogleSingIn.js
+++ src/config/firebase/GoogleSingIn.js
@@ -7,17 +7,17 @@ export const _signInWithGoogle = async () => {
       offlineAccess: false,
       scopes: ['profile', 'email'],
       webClientId:
-        '144812370315-hcm19e9ici4g5ou5qp0gv9ube20pj48r.apps.googleusercontent.com',
+        '144812370315-ul8klb8dd4cgcb7uo4q0qqqea31ghet4.apps.googleusercontent.com',
     });
     await GoogleSignin.hasPlayServices();
     const userInfo = await GoogleSignin.signIn();
-
+    // this.setState({ userInfo });
     const {idToken} = await GoogleSignin.signIn();
     const googleCredentials = auth.GoogleAuthProvider.credential(idToken);
     auth().signInWithCredential(googleCredentials);
     return userInfo;
   } catch (error) {
-    console.log('=> Google Sign In', error);
+    console.log('=> Google Sign In Error:', error);
     return null;
   }
 };
